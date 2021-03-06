60a4654c98c441e5e506829b80690911036677b1
Daniel Astudillo <dma3@williams.edu>
2021-03-06 00:34:14 -0500

    finished part 1

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Daniel Astudillo on 3/5/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+extension Bundle {
+    var displayName: String? {
+        return object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? object(forInfoDictionaryKey: "CFBundleName") as? String
+    }
+}
+
+class LoginViewController: UIViewController {
+    let appName = Bundle.main.displayName!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true{
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    func continueLogin(){
+        UserDefaults.standard.set(true, forKey: "userLoggedIn")
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("could not login:")}
+        )
+    }
+    @IBAction func onLoginButton(_ sender: Any) {
+        // create the alert
+
+        let alert = UIAlertController(title: "\"\(String(describing: appName))\" Wants to Use  \n\"Twitter.com\" to Sign In.", message: "This allows the app and website to share information about you.", preferredStyle: UIAlertController.Style.alert)
+
+        // add the actions (buttons)
+        alert.addAction(UIAlertAction(title: "Continue", style: UIAlertAction.Style.default, handler: {(action: UIAlertAction) in self.continueLogin()}))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
